380b027aac732cde44068292648fe11e2e39ac59
Zero-K00L <johngrudzinski@yahoo.com>
2026-08-24 17:14:44 -0400

    feat: initialize Spring Boot API with hello endpoint

---
+++ src/test/java/com/portfolio/ticketapi/TicketapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.ticketapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
